c444ea2be3b5b356e6273991248cc1f7712de06d
Talha Ansar <talhabajwa.x1@gmail.com>
2026-05-18 08:29:59 +0500

    Add hello Python file

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello, World!");
+print("hello , Talha bro");
+print("hello , Talha Bajwa bro");
+name = input("Enter your name: ");
+print("Hello, " + name + "! Welcome to Python programming.");
